a2834e4a3206d2300b393c12e643dd28b1afd29a
Deepmala <117112464+DeepmalaDixit@users.noreply.github.com>
2023-03-19 15:53:06 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -7,27 +7,49 @@ const App = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [sortAscending, setSortAscending] = useState(true);  
 
+  const fetchData = async () => {
+    setIsLoading(true);
+    const response = await fetch('https://content.newtonschool.co/v1/pr/main/users');
+    const data = await response.json();
+    setIsLoading(false);
+    setUsers(data);
+  };
+
+  const handleSort = () => {
+    const sortedUsers = [...users].sort((a, b) => {
+      if (sortAscending) {
+        return a.name.length - b.name.length;
+      } else {
+        return b.name.length - a.name.length;
+      }
+    });
+    setUsers(sortedUsers);
+    setSortAscending(!sortAscending);
+  };
+
   return (
     <div id="main">
       <h2>User List</h2>
-      <button className="fetch-data-btn">Fetch User Data</button>
-      <button className="sort-btn">
-        "Sort by name length (ascending)"
-        "Sort by name length (descending)"
+      <button className="fetch-data-btn" onClick={fetchData}>Fetch User Data</button>
+      <button className="sort-btn" onClick={handleSort}>
+        {sortAscending ? "Sort by name length (ascending)" : "Sort by name length (descending)"}
       </button>
-      <p>Loading...</p>
-      <div className='users-section'>
-          <li>
-            <section className='id-section'></section>
-            <section className='name-email-section'>
-              <p className='name'>Name: </p>
-              <p className='email'>Email: </p>
-            </section>
-          </li>
-      </div>
+      {isLoading && <p>Loading...</p>}
+      {!isLoading && (
+        <div className='users-section'>
+          {users.map(user => (
+            <li key={user.id}>
+              <section className='id-section'>{user.id}</section>
+              <section className='name-email-section'>
+                <p className='name'>Name: {user.name}</p>
+                <p className='email'>Email: {user.email}</p>
+              </section>
+            </li>
+          ))}
+        </div>
+      )}
     </div>
   )
 }
-
 
 export default App;
